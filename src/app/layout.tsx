fd5c55c65622eed6686f4a4044d24677ff369058
angel melendez <angelmelen88@gmail.com>
2025-01-14 15:59:13 -0600

    feat:add styles global y update componente Header styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "app/Components/Shared/Header";
 import { Footer } from "app/Components/Shared/Footer";
+import "../Sass/globals.sass";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
