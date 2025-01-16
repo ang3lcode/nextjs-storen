2ab81675dce4d23c55ecd15aee94b0ba145a85c1
angel melendez <angelmelen88@gmail.com>
2025-01-16 11:37:21 -0600

    feat:eliminar archivos SVG no utilizados y update componente Description con imagen y text

--- src/Components/Home/Description/Description.tsx
+++ src/Components/Home/Description/Description.tsx
@@ -1,6 +1,12 @@
 
+import Image from 'next/image';
+
 export const Description = () => {
 	return (
-		<section>Description</section>
+		<section>
+			<Image src="/Image/description.jpeg" alt="products" width={500} height={300} />
+			<h2>Descripción</h2>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta corporis quo hic dolorem quas vel animi repellendus, in sunt deserunt dolore officiis odit eos. Error eius nobis provident temporibus eligendi.</p>
+		</section>
 	)
 }
