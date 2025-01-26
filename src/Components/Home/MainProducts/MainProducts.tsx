46ab3e3c660de03a6143db9a06f29da7bfeb69d8
angel melendez <angelmelen88@gmail.com>
2025-01-26 17:52:58 -0600

    feat: implementar API para obtener productos y actualizar componente MainProducts

--- src/Components/Home/MainProducts/MainProducts.tsx
+++ src/Components/Home/MainProducts/MainProducts.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-import { getProducts } from 'app/services/shopify'
+// import { getProducts } from 'app/services/shopify'
 
 
 export const MainProducts = async () => {
-	const products: { id: string; title: string; images: { src: string }[] }[] = await getProducts()
+	// const products: { id: string; title: string; images: { src: string }[] }[] = await getProducts()
 	// console.log(products)
+	const reponse = await fetch('http://localhost:3000/api')
+	const { products } = await reponse.json()
+
 
 
 	return (
