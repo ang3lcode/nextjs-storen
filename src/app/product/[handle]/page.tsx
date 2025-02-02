719b8e0f654e0f3a0a0d068df28b76d4e2d4c90f
angel melendez <angelmelen88@gmail.com>
2025-02-02 11:21:05 -0600

    feat: agregar redirección en ProductView y page.tsx para manejar productos no encontrados

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -2,6 +2,7 @@
 import { ProductView } from "app/Components/product/ProductView"
 // import { useParams, useSearchParams } from "next/navigation"
 import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
 	searchParams: {
@@ -17,6 +18,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
 	const id = searchParams.id
 	const products = await getProducts(id)
 	const product = products[0]
+
+	if (!id) {
+		redirect('/')
+	}
 
 	// console.log('searchParams', id)
 	return <ProductView product={product} />
