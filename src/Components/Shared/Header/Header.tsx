dc41c7dbc4c7885a21910b670526642281f82ecc
angel melendez <angelmelen88@gmail.com>
2025-02-13 12:18:59 -0600

    feat: implementar validación de token de acceso y mostrar nombre del cliente en el encabezado

--- src/Components/Shared/Header/Header.tsx
+++ src/Components/Shared/Header/Header.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link"
 import styles from './Header.module.css'
-import { cookies } from 'next/headers'
+import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
 
-export const Header = () => {
-	// console.log("Header");
-	const cookiesStore = cookies()
-	const token = cookiesStore.get('accessToken')?.value
+export const Header = async () => {
+	const customer = await validateAccessToken()
 
 	return (
 		<header>
@@ -27,7 +25,7 @@ export const Header = () => {
 						</Link>
 					</li>
 				</ul>
-				{token ? (<p>Hola!</p>) : (<Link href="/login">Login</Link>)}
+				{customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
 			</nav>
 		</header>
 	)
